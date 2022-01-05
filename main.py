48d96f9fd141ba8cd4156c3a5e32cc3b5fc56f2d
traceur0 <ghost01ghost@gmail.com>
2022-01-05 17:46:25 +0900

    This is the first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask("superscrapper")
+
+@app.route("/")
+def home():
+    return render_template("scrapper.html")
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", searchingBy = word)
+
+app.run(host="0.0.0.0") # initializing, releasing own website
+
+
+@app.route("/contact") # @ is decorator
+def contact(): # With sign '@', connected to the function below
+    return "Contact me!"
+
+@app.route("/<username>") # dynamic URL
+def name(username):
+    return f"Hello {username}, how are you doing?"
